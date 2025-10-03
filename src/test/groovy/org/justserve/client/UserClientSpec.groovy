cec9510bee0ac118b64b4820b0c2cb8a0ba9416c
jonathan zollinger <jonathan.zollinger@outlook.com>
2025-10-03 15:59:49 -0600

    feat: add getAdminContext

--- src/test/groovy/org/justserve/client/UserClientSpec.groovy
+++ src/test/groovy/org/justserve/client/UserClientSpec.groovy
@@ -1,12 +1,12 @@
 package org.justserve.client
 
-
 import io.micronaut.http.HttpResponse
-import io.micronaut.http.HttpStatus
 import io.micronaut.http.client.exceptions.HttpClientResponseException
 import org.justserve.JustServeSpec
 import org.justserve.model.UserHashRequestByEmail
 import spock.lang.Shared
+
+import static io.micronaut.http.HttpStatus.*
 
 class UserClientSpec extends JustServeSpec {
 
@@ -17,6 +17,34 @@ class UserClientSpec extends JustServeSpec {
     def setupSpec() {
         noAuthUserClient = noAuthCtx.getBean(UserClient)
         userClient = ctx.getBean(UserClient)
+    }
+
+    def "get admin context for #description"() {
+        when:
+        def response = null
+        def thrownException = null
+        try {
+            response = client.getAdminContext(UUID.fromString(uid))
+        } catch (HttpClientResponseException e) {
+            thrownException = e
+        }
+
+        then:
+        if (thrownException) {
+            thrownException.status == expectedStatus
+        } else {
+            response.status == expectedStatus
+            response.body() != null
+            if (expectedStatus == OK) {
+                response.body().userId == UUID.fromString(uid)
+            }
+        }
+
+        where:
+        description            | uid                                    | client           | expectedStatus
+        "a valid user"         | "e23d029c-25f6-4c93-aada-dcbdc6d50c2c" | userClient       | OK
+        "an invalid user"      | faker.internet().uuid()                | userClient       | NOT_FOUND
+        "a valid user no-auth" | "e23d029c-25f6-4c93-aada-dcbdc6d50c2c" | noAuthUserClient | UNAUTHORIZED
     }
 
     def "get tempPassword for #email"() {
@@ -46,7 +74,7 @@ class UserClientSpec extends JustServeSpec {
 
         where:
         expectedResponse | email                 | expectedException           | expectedMessage  | client           | _
-        HttpStatus.OK    | userEmail             | null                        | null             | userClient       | _
+        OK               | userEmail             | null                        | null             | userClient       | _
         null             | "notanemail@mail.moc" | HttpClientResponseException | "\"status\":500" | userClient       | _
         null             | userEmail             | HttpClientResponseException | "\"status\":401" | noAuthUserClient | _
         null             | "notanemail@mail.moc" | HttpClientResponseException | "\"status\":401" | noAuthUserClient | _
