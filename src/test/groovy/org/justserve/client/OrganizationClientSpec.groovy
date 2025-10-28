276b2dbf18d261b54081f42fad02a07dccd6d756
jonathan zollinger <jonathan.zollinger@gmail.com>
2025-10-28 12:43:36 -0600

    test: add org.searchByLocation test

--- src/test/groovy/org/justserve/client/OrganizationClientSpec.groovy
+++ src/test/groovy/org/justserve/client/OrganizationClientSpec.groovy
@@ -4,16 +4,41 @@ import io.micronaut.http.HttpStatus
 import io.micronaut.http.client.exceptions.HttpClientResponseException
 import org.justserve.JustServeSpec
 import org.justserve.model.OrganizationCreateRequest
+import org.justserve.model.OrganizationSearchRequest
 import spock.lang.Shared
 
 class OrganizationClientSpec extends JustServeSpec {
     @Shared
     OrganizationClient noAuthClient, authClient
 
+    @Shared
+    DynamicRoutingClient dynamicRoutingClient
+
 
     def setupSpec() {
         noAuthClient = noAuthCtx.getBean(OrganizationClient)
         authClient = ctx.getBean(OrganizationClient)
+        dynamicRoutingClient = ctx.getBean(DynamicRoutingClient)
+    }
+
+    def "using searchByLocation() should work when using #title"() {
+        when:
+        def search = new OrganizationSearchRequest()
+                .setLocation("Elk Grove, CA 95758, USA")
+                .setSortBy("az")
+        def response = client.searchByLocation(search)
+
+        then:
+        response.status() == expectedStatus
+        if (expectedStatus == HttpStatus.OK) {
+            response.body() != null
+            response.body().organizations.size() > 0
+        }
+
+        where:
+        expectedStatus | client       | title
+        HttpStatus.OK  | authClient   | "auth client"
+        HttpStatus.OK  | noAuthClient | "no auth client"
     }
 
     def "can add org using #title"() {
