44f9f6c4f59d4dd14842adfdbcc03a8545b3383b
jonathan zollinger <jonathan.zollinger@gmail.com>
2025-10-28 13:02:30 -0600

    refactor: move re-used code to methods

--- src/test/groovy/org/justserve/client/OrganizationClientSpec.groovy
+++ src/test/groovy/org/justserve/client/OrganizationClientSpec.groovy
@@ -29,9 +29,7 @@ class OrganizationClientSpec extends JustServeSpec {
 
     def "using searchByLocation() should work when using #title"() {
         when:
-        def search = new OrganizationSearchRequest()
-                .setLocation("Elk Grove, CA 95758, USA")
-                .setSortBy("az")
+        def search = createSearchRequestForElkGrove()
         def response = client.searchByLocation(search)
 
         then:
@@ -49,9 +47,7 @@ class OrganizationClientSpec extends JustServeSpec {
 
     def "get admins for a given org with no error"() {
         given:
-        def search = new OrganizationSearchRequest()
-                .setLocation("Elk Grove, CA 95758, USA")
-                .setSortBy("az")
+        def search = createSearchRequestForElkGrove()
         UUID orgID = client.searchByLocation(search).body().organizations.first.id
 
         when:
@@ -68,31 +64,19 @@ class OrganizationClientSpec extends JustServeSpec {
 
     def "create an org with no error"() {
         given:
-        HttpResponse<ImageUploadResponse> profileImage = authImageClient.uploadImage(
-                new ImageUploadRequest(faker.image().base64JPG().split(",")[1], 256, 256, false, 0, 0)
-        )
-        //check that slug isn't already used
-        String urlSlug = null
-        while (null == urlSlug) {
-            def potentialSlug = faker.word().noun()
-            def response = dynamicRoutingClient.getOrgIdFromSlug(potentialSlug)
-            if (response.status() == HttpStatus.NOT_FOUND) {
-                urlSlug = potentialSlug
-            }
-        }
-        when:
         def orgRequest = new OrganizationCreateRequest()
                 .setContactEmail(faker.internet().emailAddress())
                 .setContactName(faker.zelda().character())
                 .setContactPhone(faker.phoneNumber().phoneNumber())
                 .setDescription(faker.zelda().game())
                 .setLocationString(knownWorkingLocation)
-                .setLogo(profileImage.body().displayFileName)
+                .setLogo(getUploadedImageFileName())
                 .setName(faker.zelda().character())
                 .set_public(null)
-                .setUrl(urlSlug)
+                .setUrl(getUniqueSlug())
                 .setVolunteerCenterInfo(null)
                 .setWebsite(faker.internet().url())
+        when:
         authClient.createOrganization(orgRequest)
 
         then:
@@ -101,26 +85,14 @@ class OrganizationClientSpec extends JustServeSpec {
 
     def "create an org with with #title and expected results"() {
         given:
-        def profileImage = authImageClient.uploadImage(
-                new ImageUploadRequest(faker.image().base64JPG().split(",")[1], 256, 256, false, 0, 0)
-        )
-        String urlSlug = null
-        while (null == urlSlug) {
-            def potentialSlug = faker.word().noun()
-            def slugQueryResponse = dynamicRoutingClient.getOrgIdFromSlug(potentialSlug)
-            if (slugQueryResponse.status() == HttpStatus.NOT_FOUND) {
-                urlSlug = potentialSlug
-            }
-
-        }
         def orgCreationRequest = new OrganizationCreateRequest()
-                .setLogo(profileImage.body().displayFileName)
+                .setLogo(getUploadedImageFileName())
                 .setContactEmail(faker.internet().emailAddress())
                 .setDescription(faker.chuckNorris().fact())
                 .setLocationString(knownWorkingLocation)
                 .setName(faker.company().name())
                 .set_public(true)
-                .setUrl(urlSlug)
+                .setUrl(getUniqueSlug())
 
         when:
         def response = client.createOrganization(orgCreationRequest)
@@ -137,5 +109,42 @@ class OrganizationClientSpec extends JustServeSpec {
         expectedStatus     | client       | title
         HttpStatus.CREATED | authClient   | "auth client"
         HttpStatus.CREATED | noAuthClient | "no auth client"
+    }
+
+    /**
+     * Creates a default organization search request for testing.
+     * @return A pre-configured {@link OrganizationSearchRequest}.
+     */
+    private static OrganizationSearchRequest createSearchRequestForElkGrove() {
+        return new OrganizationSearchRequest()
+                .setLocation("Elk Grove, CA 95758, USA")
+                .setSortBy("az")
+    }
+
+    /**
+     * Uploads a fake JPG image and returns the display file name.
+     * @return The file name of the uploaded image.
+     */
+    private String getUploadedImageFileName() {
+        HttpResponse<ImageUploadResponse> profileImage = authImageClient.uploadImage(
+                new ImageUploadRequest(faker.image().base64JPG().split(",")[1], 256, 256, false, 0, 0)
+        )
+        return profileImage.body().displayFileName
+    }
+
+    /**
+     * Generates a unique URL slug for an organization by checking for its existence.
+     * @return A unique string to be used as a URL slug.
+     */
+    private String getUniqueSlug() {
+        String urlSlug = null
+        while (null == urlSlug) {
+            def potentialSlug = faker.word().noun()
+            def response = dynamicRoutingClient.getOrgIdFromSlug(potentialSlug)
+            if (response.status() == HttpStatus.NOT_FOUND) {
+                urlSlug = potentialSlug
+            }
+        }
+        return urlSlug
     }
 }
