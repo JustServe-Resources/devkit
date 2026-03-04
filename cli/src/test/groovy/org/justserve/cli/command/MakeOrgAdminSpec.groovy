c1b2512818f557b4ae17edcf5cfdd04b81dc5df5
jonathan zollinger <jonathan.zollinger@gmail.com>
2026-03-04 11:13:13 -0700

    refactor(test): reuse user and orgs between tests

--- cli/src/test/groovy/org/justserve/cli/command/MakeOrgAdminSpec.groovy
+++ cli/src/test/groovy/org/justserve/cli/command/MakeOrgAdminSpec.groovy
@@ -1,20 +1,28 @@
 package org.justserve.cli.command
 
 import io.micronaut.context.ApplicationContext
-import spock.lang.Execution
+import spock.lang.Shared
 
-import static org.spockframework.runtime.model.parallel.ExecutionMode.SAME_THREAD
-
-@Execution(SAME_THREAD)
+//@Execution(SAME_THREAD)
 class MakeOrgAdminSpec extends BaseCommandSpec {
+
+    @Shared
+    UUID sharedUserID
+
+    @Shared
+    List<UUID> sharedOrgs
+
+    def setupSpec() {
+        sharedUserID = createUser().body().id
+        sharedOrgs = createTestOrgs(3)
+    }
 
     def "can make a user an admin to #orgCount org(s) using the #orgFlag and #userFlag flags #title"() {
         given:
-        UUID userID = createUser().body().id
-        def orgs = createTestOrgs(orgCount).join(",")
+        def orgs = sharedOrgs.take(orgCount).join(",")
 
         when:
-        def (outputStream, errorStream) = executeCommand(context as ApplicationContext, ["makeOrgAdmin", orgFlag, orgs, userFlag, userID] as String[])
+        def (outputStream, errorStream) = executeCommand(context as ApplicationContext, ["makeOrgAdmin", orgFlag, orgs, userFlag, sharedUserID] as String[])
 
         then:
         if (context == noAuthCtx) {
@@ -25,7 +33,7 @@ class MakeOrgAdminSpec extends BaseCommandSpec {
             return
         }
         verifyAll {
-            (outputStream as String).contains("successfully reassigned ${orgCount} orgs to user ${userID}")
+            (outputStream as String).contains("successfully reassigned ${orgCount} orgs to user ${sharedUserID}")
             errorStream.matches(blankRegex)
         }
 
@@ -47,22 +55,21 @@ class MakeOrgAdminSpec extends BaseCommandSpec {
 
     def "can make a user an admin to #orgCount where at least one org ID does not exist on JustServe"() {
         given:
-        UUID userID = createUser().body().id
         String orgs
         def fakeId = faker.internet().uuid().toString()
         if (orgCount == 1) {
             orgs = fakeId
         } else {
-            orgs = createTestOrgs(orgCount - 1).join(",") + "," + fakeId
+            orgs = sharedOrgs.take(orgCount - 1).join(",") + "," + fakeId
         }
 
         when:
-        def (outputStream, errorStream) = executeCommand(context as ApplicationContext, ["makeOrgAdmin", orgFlag, orgs, userFlag, userID] as String[])
+        def (outputStream, errorStream) = executeCommand(context as ApplicationContext, ["makeOrgAdmin", orgFlag, orgs, userFlag, sharedUserID] as String[])
 
         then:
         verifyAll {
-            (outputStream as String).contains("successfully reassigned ${orgCount - 1 } orgs to user ${userID}")
-            (errorStream as String).contains("Failed to make user ${userID} an admin for organization ${fakeId}.")
+            (outputStream as String).contains("successfully reassigned ${orgCount - 1} orgs to user ${sharedUserID}")
+            (errorStream as String).contains("Failed to make user ${sharedUserID} an admin for organization ${fakeId}.")
         }
 
         where:
@@ -76,24 +83,32 @@ class MakeOrgAdminSpec extends BaseCommandSpec {
     }
 
 
-
     def "can make a user an admin to #orgCount where at least one org Slug does not exist on JustServe"() {
         given:
-        UUID userID = createUser().body().id
         String orgs
         def fakeSlug = faker.internet().slug().toString()
         if (orgCount == 1) {
             orgs = fakeSlug
         } else {
-            orgs=authOrgClient.searchByLocation(createSearchRequestForElkGrove()).body().getOrganizations().url.take(orgCount - 1).join(",")+","+fakeSlug
+            // We can't easily get slugs for the sharedOrgs since they are just UUIDs in the list.
+            // However, the original test was searching for orgs by location to get slugs.
+            // To keep it fast, we can just fetch one set of slugs in setupSpec if needed,
+            // or just do the search once here if we really need slugs.
+            // But wait, createTestOrgs returns UUIDs.
+            // The original test used: authOrgClient.searchByLocation(...).getOrganizations().url
+            // Let's just do that search once in the test method, but limit it.
+            // Actually, better yet, let's just use the search here but only call it if orgCount > 1.
+            // Since we are optimizing, let's try to reuse the search result if possible, but for now
+            // let's stick to the original logic for the slug part but reuse the user.
+            orgs = authOrgClient.searchByLocation(createSearchRequestForElkGrove()).body().getOrganizations().url.take(orgCount - 1).join(",") + "," + fakeSlug
         }
 
         when:
-        def (outputStream, errorStream) = executeCommand(context as ApplicationContext, ["makeOrgAdmin", orgFlag, orgs, userFlag, userID] as String[])
+        def (outputStream, errorStream) = executeCommand(context as ApplicationContext, ["makeOrgAdmin", orgFlag, orgs, userFlag, sharedUserID] as String[])
 
         then:
         verifyAll {
-            (outputStream as String).contains("successfully reassigned ${orgCount - 1 } orgs to user ${userID}")
+            (outputStream as String).contains("successfully reassigned ${orgCount - 1} orgs to user ${sharedUserID}")
             (errorStream as String).contains("Error The org '${fakeSlug}' is not found on JustServe")
         }
 
