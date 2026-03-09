50b6ece85225035b9057558fcffb86f88fb2b5dd
Jonathan Zollinger <62955101+Jonathan-Zollinger@users.noreply.github.com>
2026-03-09 12:57:15 -0600

    add graph api calls (#73)
    most of these still need testing.

---
+++ core/src/test/groovy/org/justserve/GraphQLClientSpec.groovy
@@ -0,0 +1,36 @@
+package org.justserve
+
+import io.micronaut.test.extensions.spock.annotation.MicronautTest
+import jakarta.inject.Inject
+import org.justserve.client.GraphQLClient
+import org.justserve.model.EventType
+import org.justserve.model.GraphQLCreateProjectVariables
+import org.justserve.model.ProjectLocationType
+import spock.lang.Shared
+import spock.lang.Specification
+
+@MicronautTest
+class GraphQLClientSpec extends Specification {
+
+    @Shared
+    @Inject
+    GraphQLClient client
+
+    void "can create Project with EventType: #eventType, LocationType: #locationType, and Redirect: #redirect"(EventType eventType, ProjectLocationType locationType, String redirect) {
+        given:
+        GraphQLCreateProjectVariables args = new GraphQLCreateProjectVariables()
+                .setEventType(eventType)
+                .setLocationType(locationType)
+                .setTitle("this is my title")
+                .setRedirect(redirect)
+
+        when:
+        client.createProject(args)
+
+        then:
+        noExceptionThrown()
+
+        where:
+        [eventType, locationType, redirect] << [EventType.values(), ProjectLocationType.values(), ["", null, "https://google.com"]].combinations()
+    }
+}
