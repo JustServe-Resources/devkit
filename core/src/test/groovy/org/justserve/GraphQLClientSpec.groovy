571f1b7b5f1ca095909dad68cf309231805a2bdb
jonathan zollinger <jonathan.zollinger@gmail.com>
2026-03-09 12:27:12 -0600

    test: add createProject test for graph client

--- core/src/test/groovy/org/justserve/GraphQLClientSpec.groovy
+++ core/src/test/groovy/org/justserve/GraphQLClientSpec.groovy
@@ -1,20 +1,36 @@
 package org.justserve
 
+import io.micronaut.test.extensions.spock.annotation.MicronautTest
 import jakarta.inject.Inject
 import org.justserve.client.GraphQLClient
-import org.justserve.client.GraphQLRequest
-import org.justserve.model.GraphQLCreateProjectRequest
+import org.justserve.model.EventType
+import org.justserve.model.GraphQLCreateProjectVariables
+import org.justserve.model.ProjectLocationType
 import spock.lang.Shared
 import spock.lang.Specification
 
-class GraphQLClientSpec extends Specification{
+@MicronautTest
+class GraphQLClientSpec extends Specification {
 
     @Shared
     @Inject
-    private GraphQLClient client;
+    GraphQLClient client
 
-    void "simple test"(){
-        GraphQLRequest request = new GraphQLCreateProjectRequest()
-        client.createProject()
+    void "can create Project with EventType: #eventType, LocationType: #locationType, and Redirect: #redirect"(EventType eventType, ProjectLocationType locationType, String redirect) {
+        given:
+        GraphQLCreateProjectVariables args = new GraphQLCreateProjectVariables()
+                .setEventType(eventType)
+                .setLocationType(locationType)
+                .setTitle("this is my title")
+                .setRedirect(redirect)
+
+        when:
+        client.createProject(args)
+
+        then:
+        noExceptionThrown()
+
+        where:
+        [eventType, locationType, redirect] << [EventType.values(), ProjectLocationType.values(), ["", null, "https://google.com"]].combinations()
     }
 }
