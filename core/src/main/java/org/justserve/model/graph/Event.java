a59bcab148a417add5be4cb8ed72d3cecc4c95a8
jonathan zollinger <jonathan.zollinger@gmail.com>
2026-03-10 16:38:26 -0600

    refactor(wip): build mutation query
    variables are not sent properly

---
+++ core/src/main/java/org/justserve/model/graph/Event.java
@@ -0,0 +1,129 @@
+package org.justserve.model.graph;
+
+import io.micronaut.core.annotation.Introspected;
+import io.micronaut.core.annotation.Nullable;
+import io.micronaut.serde.annotation.Serdeable;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import java.time.ZonedDateTime;
+import java.util.UUID;
+
+/**
+ * <h4>All potential variables to use with {@link CreateEventQuery}</h4>
+ * Null values will be omitted from the generated query.
+ *
+ * @since 0.1.0
+ * @author Jonathan Zollinger
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data
+@Accessors(chain = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@Serdeable
+@Introspected
+public class Event extends GraphVariables {
+    @Nullable
+    @Email
+    private String contactEmail;
+
+    @Nullable
+    @Size(max = 139)
+    private String contactName;
+
+    @Nullable
+    private String contactPhone;
+
+    @Nullable
+    private Boolean deleted;
+
+    @Nullable
+    private UUID deletedBy;
+
+    @Nullable
+    private ZonedDateTime deletedOn;
+
+    @Nullable
+    private ZonedDateTime end;
+
+    @Nullable
+    private ZonedDateTime endDateTimeOffset;
+
+    @Nullable
+    private Boolean eventCapReached;
+
+    @Nullable
+    private Integer groupCap;
+
+    @Nullable
+    private Integer groupLimit;
+
+    @Nullable
+    private UUID id;
+
+    @Nullable
+    private String locationLink;
+
+    @Nullable
+    @Size(max = 139)
+    private String locationName;
+
+    @Nullable
+    private UUID projectEventLocationId;
+
+    @Nullable
+    private UUID projectId;
+
+    @Nullable
+    private UUID projectRecurringTimeId;
+
+    @Nullable
+    private String qrCodeImageLocation;
+
+    @Nullable
+    private ZonedDateTime renewDate;
+
+    @Nullable
+    @Size(max = 300)
+    private String schedule;
+
+    @Nullable
+    @Size(max = 300)
+    private String shiftTitle;
+
+    @Nullable
+    private String specialDirections;
+
+    @Nullable
+    private ZonedDateTime start;
+
+    @Nullable
+    private ZonedDateTime startDateTimeOffset;
+
+    @Nullable
+    private String status;
+
+    @Nullable
+    private UUID statusId;
+
+    @Nullable
+    private String timezone;
+
+    @Nullable
+    private UUID timeZoneEnumId;
+
+    @Nullable
+    private Integer totalVolunteersNeeded;
+
+    @Nullable
+    private Integer volunteerCap;
+
+    @Nullable
+    private Integer volunteersNeeded;
+}
