62c0db86338d07bdcca1afee5a34902cc8616ab7
Jonathan Zollinger <jonathan.zollinger@gmail.com>
2026-03-11 16:04:20 -0600

    test: lean into providing fake data
    Signed-off-by: jonathan zollinger <jonathan.zollinger@gmail.com>

--- core/src/test/groovy/org/justserve/GraphQLClientSpec.groovy
+++ core/src/test/groovy/org/justserve/GraphQLClientSpec.groovy
@@ -2,6 +2,7 @@ package org.justserve
 
 import io.micronaut.test.extensions.spock.annotation.MicronautTest
 import jakarta.inject.Inject
+import net.datafaker.Faker
 import org.justserve.client.GraphQLClient
 import org.justserve.model.EventType
 import org.justserve.model.GraphQLCreateProjectVariables
@@ -13,7 +14,7 @@ import spock.lang.Shared
 import spock.lang.Specification
 import spock.lang.Unroll
 
-import java.time.ZonedDateTime
+import java.util.concurrent.TimeUnit
 
 @MicronautTest
 class GraphQLClientSpec extends Specification {
@@ -21,6 +22,9 @@ class GraphQLClientSpec extends Specification {
     @Shared
     @Inject
     GraphQLClient client
+
+    @Shared
+    Faker faker = new Faker()
 
     void "can create Project with EventType: #eventType, LocationType: #locationType, and Redirect: #redirect"(EventType eventType, ProjectLocationType locationType, String redirect) {
         given:
@@ -47,7 +51,7 @@ class GraphQLClientSpec extends Specification {
         def projectId = client.createProject(new GraphQLCreateProjectVariables()
                 .setTitle("this is a test")
                 .setEventType(EventType.Ongoing)
-                .setLocationType(ProjectLocationType.SINGLE_location)
+                .setLocationType(ProjectLocationType.SINGLE_LOCATION)
         ).data.createProject.id
 
         def event = new CreateEventFields()
@@ -82,32 +86,61 @@ class GraphQLClientSpec extends Specification {
         def response = client.createEvent(query)
 
         then:
+        if (response.hasErrors()) {
+            def errorLog = """
+            ${response.errors}
+            Errors for combination:
+            contactEmail: ${contactEmail}
+            contactName: ${contactName}
+            contactPhone: ${contactPhone}
+            deleted: ${deleted}
+            end: ${end}
+            eventCapReached: ${eventCapReached}
+            groupCap: ${groupCap}
+            groupLimit: ${groupLimit}
+            locationLink: ${locationLink}
+            locationName: ${locationName}
+            qrCodeImageLocation: ${qrCodeImageLocation}
+            renewDate: ${renewDate}
+            schedule: ${schedule}
+            shiftTitle: ${shiftTitle}
+            specialDirections: ${specialDirections}
+            start: ${start}
+            status: ${status}
+            timezone: ${timezone}
+            totalVolunteersNeeded: ${totalVolunteersNeeded}
+            volunteerCap: ${volunteerCap}
+            volunteersNeeded: ${volunteersNeeded}
+            
+            """
+            new File('build/test-errors.log').append(errorLog)
+        }
         noExceptionThrown()
         !response.hasErrors()
 
         where:
         [contactEmail, contactName, contactPhone, deleted, end, eventCapReached, groupCap, groupLimit, locationLink, locationName, qrCodeImageLocation, renewDate, schedule, shiftTitle, specialDirections, start, status, timezone, totalVolunteersNeeded, volunteerCap, volunteersNeeded] << [
-                ["test@test.com", "a@a.com", null],
-                ["name", "another name", null],
-                ["123-456-7890", "9876543210", null],
+                [faker.internet().emailAddress(), null],
+                [faker.name().fullName(), null],
+                [faker.phoneNumber().phoneNumber(), null],
                 [true, false, null],
-                [Date.from(ZonedDateTime.now().plusMonths(6).toInstant()), Date.from(ZonedDateTime.now().plusYears(1).toInstant()), null],
+                [faker.timeAndDate().future(365, TimeUnit.DAYS), null],
                 [true, false, null],
-                [10, 20, null],
-                [5, 10, null],
-                ["https://google.com", "https://bing.com", null],
-                ["location", "another location", null],
-                ["/qr/code.png", "/another/qr.png", null],
-                [Date.from(ZonedDateTime.now().plusYears(1).toInstant()), Date.from(ZonedDateTime.now().plusYears(2).toInstant()), null],
-                ["schedule", "another schedule", null],
-                ["shift", "another shift", null],
-                ["directions", "more directions", null],
-                [Date.from(ZonedDateTime.now().plusMonths(1).toInstant()), Date.from(ZonedDateTime.now().plusDays(15)), null],
-                ["active", "inactive", null],
-                ["UTC", "America/New_York", null],
-                [100, 200, null],
-                [50, 100, null],
-                [25, 50, null]
+                [(+faker.number()), null],
+                [(+faker.number()), null],
+                [faker.internet().url(), null],
+                [faker.address().fullAddress(), null],
+                [faker.internet().url(), null],
+                [faker.timeAndDate().future(730, TimeUnit.DAYS), null],
+                [faker.lorem().sentence(), null],
+                [faker.lorem().sentence(), null],
+                [faker.lorem().paragraph(), null],
+                [faker.timeAndDate().future(180, TimeUnit.DAYS), null],
+                [faker.lorem().word(), null],
+                [faker.address().timeZone(), null],
+                [(+faker.number()), null],
+                [(+faker.number()), null],
+                [(+faker.number()), null]
         ].combinations()
     }
 }
