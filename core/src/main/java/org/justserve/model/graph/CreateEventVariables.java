a156da1c22b83c7ea1913231a2b399eb6904403c
jonathan zollinger <jonathan.zollinger@gmail.com>
2026-03-12 15:59:14 -0600

    fix: add missing CreateEventVariables
    Signed-off-by: jonathan zollinger <jonathan.zollinger@gmail.com>

---
+++ core/src/main/java/org/justserve/model/graph/CreateEventVariables.java
@@ -0,0 +1,27 @@
+package org.justserve.model.graph;
+
+import io.micronaut.serde.annotation.Serdeable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import java.util.UUID;
+
+/**
+ * Pojo to serialize the variables object passed with a createEvent mutation.
+ *
+ * @author Jonathan Zollinger
+ * @since 0.1.0
+ */
+@EqualsAndHashCode(callSuper = true)
+@Serdeable
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Accessors(chain = true)
+public class CreateEventVariables extends GraphVariables {
+    private UUID projectId;
+    private ProjectEvent projectEvent;
+}
