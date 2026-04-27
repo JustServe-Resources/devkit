93bf7c1fb280de116a6f2703698523af8e60e69c
jonathan zollinger <jonathan.zollinger@gmail.com>
2026-04-27 13:27:08 -0600

    refactor(test): separate tests
    move success and failures to their own tests

--- core/src/test/groovy/org/justserve/BoundaryPermissionSpec.groovy
+++ core/src/test/groovy/org/justserve/BoundaryPermissionSpec.groovy
@@ -5,7 +5,8 @@ import io.micronaut.test.extensions.spock.annotation.MicronautTest
 import org.justserve.client.BoundaryPermissionClient
 import spock.lang.Shared
 
-import static io.micronaut.http.HttpStatus.INTERNAL_SERVER_ERROR
+import static io.micronaut.http.HttpStatus.FORBIDDEN
+import static io.micronaut.http.HttpStatus.UNAUTHORIZED
 
 @MicronautTest
 class BoundaryPermissionSpec extends JustServeSpec {
@@ -18,28 +19,33 @@ class BoundaryPermissionSpec extends JustServeSpec {
         authBoundaryPermissionClient = ctx.getBean(BoundaryPermissionClient)
     }
 
-    def "can reassign organizations #title"() {
+    def "successfully reassign organization admin"() {
         given:
         UUID userID = createUser().getId()
+        UUID orgID = createOrg()
 
         when:
         authBoundaryPermissionClient.makeAdminForOrg(orgID, userID).block()
 
         then:
-        if (!expectedError) {
-            verifyAll {
-                authOrgClient.getOrgOwners(orgID).block().stream().anyMatch { user -> (user.id == userID) }
-            }
-            return
-        }
+        authOrgClient.getOrgOwners(orgID).block().stream().anyMatch { user -> (user.id == userID) }
+    }
+
+    def "fail to reassign organization admin #title"() {
+        given:
+        UUID userID = createUser().getId()
+
+        when:
+        client.makeAdminForOrg(orgID, userID).block()
+
+        then:
         def exception = thrown(HttpClientResponseException)
         exception.status == expectedError
 
         where:
-        client                         | expectedError         | title                                                    | orgID                                               | _
-        authBoundaryPermissionClient   | null                  | "with a good OrgID as an admin and the request succeeds" | createOrg()                                         | _
-        noAuthBoundaryPermissionClient | INTERNAL_SERVER_ERROR | "with a bad OrgID as an admin and the request fails"     | UUID.fromString(faker.internet().uuid().toString()) | _
-//        noAuthBoundaryPermissionClient | UNAUTHORIZED  | "as an unauthorized user and the request fails" | createOrg() | _
+        client                         | expectedError | orgID             | title
+        noAuthBoundaryPermissionClient | UNAUTHORIZED  | createOrg()       | "as an unauthorized user"
+        authBoundaryPermissionClient   | FORBIDDEN     | UUID.randomUUID() | "with a non-existent OrgID"
     }
 
 
