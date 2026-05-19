04c1efb757a0ba6e1e92ff8bcd311fc9e9467c73
ZeterTheDuck <126122462+ZeterTheDuck@users.noreply.github.com>
2026-05-18 21:52:15 -0600

    Introduce Location and Language Clients

---
+++ core/src/test/groovy/org/justserve/LocationSpec.groovy
@@ -0,0 +1,41 @@
+package org.justserve
+
+import io.micronaut.test.extensions.spock.annotation.MicronautTest
+import org.justserve.client.LanguageClient
+import org.justserve.client.LocationClient
+import spock.lang.Shared
+import spock.lang.Unroll
+
+@MicronautTest()
+class LocationSpec extends JustServeSpec {
+
+    @Shared
+    LocationClient noAuthLocationClient, locationClient
+
+    @Shared
+    LanguageClient languageClient
+
+    @Shared
+    List<String> threeCharLocales
+
+    def setupSpec() {
+        noAuthLocationClient = noAuthCtx.getBean(LocationClient)
+        locationClient = ctx.getBean(LocationClient)
+        languageClient = ctx.getBean(LanguageClient)
+        threeCharLocales = languageClient.getLanguages().block().collect { locale -> locale.getLang3char() }
+    }
+
+    @SuppressWarnings("GroovyAssignabilityCheck")
+    @Unroll ("Can query LocationClient.getLocation(#lang) with no error as #userType")
+    def "can query location with by a given language"(LocationClient client, String userType, String lang){
+        when:
+        client.getLocation(lang).block()
+
+        then:
+        noExceptionThrown()
+
+        where:
+        [[client, userType], lang] << [[[noAuthLocationClient, "no auth"], [locationClient, "standard"]], threeCharLocales].combinations()
+    }
+
+}
